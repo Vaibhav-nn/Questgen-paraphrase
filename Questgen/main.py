4679021797c03e9a6bf10524198426b3516778ba
Parth Chokhra <35425925+parthplc@users.noreply.github.com>
2020-08-26 18:35:56 +0530

    Update main.py

--- Questgen/main.py
+++ Questgen/main.py
@@ -282,6 +282,8 @@ class AnswerPredictor:
 
         encoding = self.tokenizer.encode_plus(input, return_tensors="pt")
         input_ids, attention_masks = encoding["input_ids"].to(self.device), encoding["attention_mask"].to(self.device)
-        output = greedy_decoding(input_ids,attention_masks,self.model,self.tokenizer)
+        greedy_output = self.model.generate(input_ids=input_ids, attention_mask=attention_mask, max_length=256)
+        Question =  self.tokenizer.decode(greedy_output[0], skip_special_tokens=True,clean_up_tokenization_spaces=True)
+        output = Question.strip().capitalize()
 
         return output
