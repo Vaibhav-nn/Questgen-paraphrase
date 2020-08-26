566f4e15a1bac57475ec83c88b851623d20e3ca1
Vaibhav Tiwari <42914799+Vaibhav-nn@users.noreply.github.com>
2020-08-26 17:58:30 +0530

    Update __init__.py

--- Questgen/__init__.py
+++ Questgen/__init__.py
@@ -2,5 +2,4 @@
 from __future__ import absolute_import
 from Questgen.encoding import encoding
 from Questgen.mcq import mcq
-from Questgen.train import train_gpu
 from Questgen.main import QGen, BoolQGen, AnswerPredictor
