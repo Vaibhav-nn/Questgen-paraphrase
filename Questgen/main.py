0ac13870b0124bfd4dcee430f6c75d36c8fde0f8
Vaibhav Tiwari <42914799+Vaibhav-nn@users.noreply.github.com>
2020-08-26 18:05:08 +0530

    Update main.py

--- Questgen/main.py
+++ Questgen/main.py
@@ -25,12 +25,12 @@ from nltk.corpus import brown
 from similarity.normalized_levenshtein import NormalizedLevenshtein
 from nltk.tokenize import sent_tokenize
 from flashtext import KeywordProcessor
-from Questgen.encoding import beam_search_decoding
-from Questgen.mcq import tokenize_sentences
-from Questgen.mcq import get_keywords
-from Questgen.mcq import get_sentences_for_keyword
-from Questgen.mcq import generate_questions_mcq
-from Questgen.mcq import generate_normal_questions
+from Questgen.encoding.encoding import beam_search_decoding
+from Questgen.mcq.mcq import tokenize_sentences
+from Questgen.mcq.mcq import get_keywords
+from Questgen.mcq.mcq import get_sentences_for_keyword
+from Questgen.mcq.mcq import generate_questions_mcq
+from Questgen.mcq.mcq import generate_normal_questions
 import time
 
 class QGen:
