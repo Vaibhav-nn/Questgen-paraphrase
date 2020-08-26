fc7ac20a1aaee7d57624156e66f49f4e33a017bb
Vaibhav Tiwari <42914799+Vaibhav-nn@users.noreply.github.com>
2020-08-26 17:56:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='Questgen',
-      version='2.0.11',
+      version='2.0.12',
       description='Question generator from any text',
       author='Questgen contributors',
       author_email='vaibhavtiwarifu@gmail.com',
