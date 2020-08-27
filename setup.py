cc17edba797b1f9a167c06bab375f0ab0ea5bf1f
Vaibhav Tiwari <42914799+Vaibhav-nn@users.noreply.github.com>
2020-08-27 15:48:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from distutils.core import setup
 
 setup(name='Questgen',
-      version='2.0.15',
+      version='2.0.16',
       description='Question generator from any text',
       author='Questgen contributors',
       author_email='vaibhavtiwarifu@gmail.com',
-      license='gnu',
       packages=['Questgen', 'Questgen.encoding', 'Questgen.mcq'],
       url="https://github.com/ramsrigouthamg/Questgen.ai",
       install_requires=[
